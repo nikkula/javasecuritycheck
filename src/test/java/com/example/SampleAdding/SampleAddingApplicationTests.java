74218206e74c4e3289e53bece84802f1090e8783
Uttar <Uttarkumar.nikkula@peerislands.io>
2024-08-08 16:42:22 -0500

    corrected the unit test

--- src/test/java/com/example/SampleAdding/SampleAddingApplicationTests.java
+++ src/test/java/com/example/SampleAdding/SampleAddingApplicationTests.java
@@ -12,14 +12,14 @@ class SampleAddingApplicationTests {
 
 	// @Test 
 	// void FailTestCase() {
-    //    int result = App.getSum(11, 12);
+    //    int result = SampleAddingApplication.getSum(11, 12);
     //    int expected = 28;
     //    assertEquals(expected,result);
     // }
     
     @Test 
 	void getSum() {
-       int result = App.getSum(10, 12);
+       int result = SampleAddingApplication.getSum(10, 12);
        int expected = 22;
        assertEquals(expected,result);
     }
